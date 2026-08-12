185e09c4f45ecf8a77269309af1c65e2a6fd7f4c
Ed333w <edward0308102@gmail.com>
2026-08-12 07:22:33 -0500

    Create arreglos.py

---
+++ Corte 1/arreglos.py
@@ -0,0 +1,17 @@
+#vector estatico 
+#.len()
+from statistics import mean
+
+
+#lecturas = [23.5, 24.0, 22.8, 23.9, 24.1] # vector estatico de flotantes con 5 elementos
+#lecturas.len()
+#lecturas.append(26.1)
+
+#problema: calcular el maximo, minimo y promedio e un conjunto de lecturas(datos)
+
+Gxpergame = [1.2, 2.8, 0.6, 3.4, 1.9, 2.5, 0.8, 1.7, 2.2, 3.0]
+
+print("Máximo:", max(Gxpergame))
+print("Mínimo:", min(Gxpergame))
+print("Promedio:", mean(Gxpergame))
+
