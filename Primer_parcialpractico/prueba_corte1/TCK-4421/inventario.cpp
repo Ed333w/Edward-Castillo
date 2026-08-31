e2092433aa56ecc48be8b69cf062ac8b86f6e7a0
Ed333w <edward0308102@gmail.com>
2026-08-31 09:13:37 -0500

    Update inventario.cpp

--- Primer_parcialpractico/prueba_corte1/TCK-4421/inventario.cpp
+++ Primer_parcialpractico/prueba_corte1/TCK-4421/inventario.cpp
@@ -22,13 +22,9 @@ public:
         delete[] historial;
     }   
 
-    //void registrar(int i, int v) { if (i >= 0 && i < n - 1) historial[i] = v; }   
-    //int  total() const { int s = 0; for (int i = 0; i < n; i++) s += historial[i]; return s; }
-    //string getCodigo() const { return codigo; }
-
-    void registrar(int i, int v) { if (i >= 0 && i < n) historial[i] = v; }
-    int  total() const { int s = 0; for (int i = 0; i < n; i++) s += historial[i]; return s; }
-    string getCodigo() const { return codigo; }     
+    void registrar(int i, int v) { if (i >= 0 && i < n) historial[i] = v; }   // Esta es la función registrar de la clase Equipo, que permite registrar un valor v en el arreglo historial en la posición i, siempre y cuando i esté dentro del rango válido (0 a n-1). Si i está fuera de este rango, no se realiza ninguna acción.
+    int total() const { int s = 0; for (int i = 0; i < n; i++) s += historial[i]; return s; }   // Esta es la función total de la clase Equipo, que calcula y devuelve la suma de todos los valores almacenados en el arreglo historial. Recorre el arreglo desde la posición 0 hasta n-1, acumulando los valores en la variable s, y finalmente retorna el total.
+    string getCodigo() const { return codigo; }   // Esta es la función getCodigo de la clase Equipo, que devuelve el valor del atributo codigo del objeto. Es una función de acceso
     
 };
 
@@ -61,8 +57,8 @@ int main() {
         cout << "TICKET CERRADO - codigo de cierre: 4421-"
              << totales[0] << totales[1] << totales[2] << suma << endl;
 
-    for (int i = 0; i < N; i++) {
-        delete equipos[i];
+    for (int i = 0; i < N; i++) {  // liberar memoria de cada objeto Equipo, se creo un For para recorrer el arreglo de punteros a objetos Equipo y liberar la memoria de cada objeto utilizando delete.
+        delete equipos[i];         //  Esto asegura que no haya fugas de memoria al final del programa.
     }
     delete[] equipos;
     delete[] totales;
